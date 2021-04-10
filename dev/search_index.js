8e2aae839d83e1bc3fe0a86b490c613851a6dc24
Documenter.jl <documenter@juliadocs.github.io>
2021-04-10 18:18:11 +0000

    build based on 5c004b5

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KongYiji","category":"page"},{"location":"#KongYiji","page":"Home","title":"KongYiji","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KongYiji]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KungIChi","category":"page"},{"location":"#KungIChi","page":"Home","title":"KungIChi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KungIChi]","category":"page"}]
 }
